d16facae8d39c24db789b0bef41299179a4b5dab
Joyce Oh <joyce.oh@live.com>
2024-09-17 18:23:21 -0400

    added key pathing

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 import generalController from './controllers/generalcontroller.js';
+import { createClient } from '@supabase/supabase-js';
+
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const app = express();
 app.use(express.json());
