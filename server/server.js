a64fb350811de09e05123723002e62f9a998c2e2
Sheena Li <xinyl61@uci.edu>
2024-09-17 14:55:58 -0700

    deleting the env file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 const app = express();
 const PORT = 8080;
 app.use('/', (req, res) => {
-    console.log(req, res);
-    console.log('HIT!');
+  console.log(req, res);
+  console.log('HIT!');
 });
 app.listen(PORT, () => `Server listening on http://localhost:${PORT}`);
