fcb4cd9c21315997b171c36024f44fe7bab72339
Ali McNamara <allisonrm95@gmail.com>
2024-09-17 18:09:02 -0400

    styled data form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-// import { useState } from 'react';
-// import reactLogo from '../src/assets/react.svg';
-// import viteLogo from '../src/public/vite.svg';
 import USMap from './components/USMap';
 import './App.css';
 function App() {
-    // const [count, setCount] = useState(0);
     return (_jsx(_Fragment, { children: _jsxs("div", { children: [_jsx("h1", { children: "Clickable US Map" }), _jsx(USMap, {})] }) }));
 }
 export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-// import { useState } from 'react';
-// import reactLogo from '../src/assets/react.svg';
-// import viteLogo from '../src/public/vite.svg';
 import USMap from './components/USMap';
-
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -20,25 +14,3 @@ function App() {
 }
 
 export default App;
-
-// <div>
-
-//         <a href='https://vitejs.dev' target='_blank'>
-//           <img src={viteLogo} className='logo' alt='Vite logo' />
-//         </a>
-//         <a href='https://react.dev' target='_blank'>
-//           <img src={reactLogo} className='logo react' alt='React logo' />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className='card'>
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className='read-the-docs'>
-//         Click on the Vite and React logos to learn more
-//       </p>
