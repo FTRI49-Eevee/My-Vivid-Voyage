7858967b52dfe731bb6d38416e48d8a626a35a67
Ali McNamara <allisonrm95@gmail.com>
2024-09-17 20:07:59 -0400

    installed vitest

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import generalController from './controllers/generalcontroller.js';
 import { createClient } from '@supabase/supabase-js';
 
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const app = express();
 app.use(express.json());
