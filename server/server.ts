7a61210ceb9c2105bc02e9313f76d38ee6906516
FT49 <ft49@FT49s-iMac.local>
2024-09-18 10:33:34 -0700

    update

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
-// import generalController from './controllers/generalcontroller.js';
+import generalController from './controllers/generalcontroller.js';
 // import { createClient } from '@supabase/supabase-js';
 import userRoutes from './routes/userRoutes.js';
 import session from 'express-session';
@@ -9,8 +9,8 @@ import { Profile, Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 dotenv.config();
 
-// const supabaseUrl = process.env.SUPABASE_URL || '';
-// const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || '';
 
 const googleClientId = process.env.GOOGLE_CLIENT_ID || '';
 const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
@@ -23,6 +23,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = 8080;
+
+interface DefaultError {
+  log: string;
+  status: number;
+  message: { err: string };
+}
 
 app.use(session({
   secret: process.env.SESSION_SECRET || '',
@@ -39,7 +45,7 @@ passport.use(new GoogleStrategy({
   callbackURL: 'http://localhost:8080/auth/google/callback',
   passReqToCallback: false, 
 },
-async (/*accessToken, refreshToken, */ profile: Profile, done: (error: any, user?: any) => void) => {
+async (accessToken, refreshToken, profile: Profile, done: (error: any, user?: any) => void) => {
   
   done(null, profile);
 }));
@@ -53,18 +59,69 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  (req, res, next) => {
+    console.log('Initiating Google Auth');
+    next();
+  },
+  passport.authenticate('google', 
+    { scope: ['profile', 'email'] })
 );
 
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('/home');
+    console.log('oauth successfull', req.user)
+    res.redirect('http://localhost:3000/home');
+  },
+  (err, req, res, next) => {
+    console.error('Authentication error:', err);
+    res.redirect('/'); 
   }
 );
 
 app.use(userRoutes);
 
+app.get('/getmap', generalController.getMap, (req: Request, res: Response) => {
+  console.log('HIT! /getmap');
+  res.status(200).send(res.locals.getMap);
+});
+
+app.post(
+  '/savemap',
+  generalController.saveMap,
+  (req: Request, res: Response) => {
+    console.log('HIT! /savemap');
+    res.status(200).send(res.locals.saveMap);
+  }
+);
+
+app.get('/db', generalController.getData, (req: Request, res: Response) => {
+  console.log('HIT! /db');
+  res.status(200).send(res.locals.getData);
+});
+
+app.post('/db', generalController.saveData, (req: Request, res: Response) => {
+  console.log('HIT! /db');
+  res.status(200).send(res.locals.saveData);
+});
+
+app.use('/', (_req, res) => {
+  res.status(404).send('Error page not found!');
+});
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  const defaultErr: DefaultError = {
+    log: 'An error was caught by the global error handler in server.ts',
+    status: 500,
+    message: {
+      err: 'An error was caught by the global error handler in server.ts',
+    },
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT}`)
