3c42867edad92a03e168c76cd5c148349633662a
Joyce Oh <joyce.oh@live.com>
2024-09-17 20:56:53 -0400

    edited

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-import USMap from './components/USMap';
-import './App.css';
-function App() {
-    return (_jsx(_Fragment, { children: _jsxs("div", { children: [_jsx("h1", { children: "Clickable US Map" }), _jsx(USMap, {})] }) }));
-}
-export default App;
