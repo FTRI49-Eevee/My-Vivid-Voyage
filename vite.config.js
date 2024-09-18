f3d154bcbeb6aef42420525b8ae9bd300128d0ea
Ali McNamara <allisonrm95@gmail.com>
2024-09-17 20:35:50 -0400

    working on configuring vitest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts', // Optional: for setup files
+    },
     server: {
         port: 3000,
         open: true,
